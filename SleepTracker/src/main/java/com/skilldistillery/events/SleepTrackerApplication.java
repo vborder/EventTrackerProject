c0ea411e3826a2602d3e8ccd56af7fea0f3cb406
Vincent Borden <vinbor81@gmail.com>
2020-06-08 10:23:32 -0400

    attempt to deploy to AWS

--- SleepTracker/src/main/java/com/skilldistillery/events/SleepTrackerApplication.java
+++ SleepTracker/src/main/java/com/skilldistillery/events/SleepTrackerApplication.java
@@ -2,12 +2,17 @@ package com.skilldistillery.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SleepTrackerApplication {
+public class SleepTrackerApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SleepTrackerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SleepTrackerApplication.class, args);
 	}
-
 }
